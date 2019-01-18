b22fa59b32d3c7399f5449ec573d927653ba3314
caijunqi99 <xiaojianhai.git>
2019-01-18 19:35:52 +0800

    修改

--- application/Oilcard/Controller/WechatController.class.php
+++ application/Oilcard/Controller/WechatController.class.php
@@ -2394,7 +2394,7 @@ class WechatController extends CommentoilcardController
         $data                = [];
         $data['signType']    = 'RSA';
         $data['appId']       = 'C9q255qIg1Zp72yI';
-        $data['merchantSn']  = 'PHT2017000000002';
+        $data['merchantSn']  = '2019000010305';
         $data['outTradeNo']  = $orderSn;
         $data['tradeType']   = 'WX';
         $data['goodsBody']   = $PayCon['body'];
